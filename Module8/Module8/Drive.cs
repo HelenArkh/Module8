293ad901138a16b17e5f69f3120ad8ad103e8ac7
HelenArkh <alena.arkh@gmail.com>
2021-01-17 13:17:29 +0300

    Создание проекта и задания для Понятие файловой системы

---
+++ Module8/Module8/Drive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module8
+{
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateNewFolder(string folderName)
+        {
+            Folders.TryAdd(folderName, new Folder());
+
+            Console.WriteLine($"Папка {folderName} создана");
+        }
+    }
+}
---
+++ Module8/Module8/Folder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module8
+{
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+}
