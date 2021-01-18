74164e17ec4b6c5d1909d6d0f6ed7883b3afff46
HelenArkh <alena.arkh@gmail.com>
2021-01-18 21:40:00 +0300

    Классы для работы с файлами

--- Module8/Module8/Program.cs
+++ Module8/Module8/Program.cs
@@ -7,89 +7,106 @@ namespace Module8
     {
         static void Main(string[] args)
         {
-            //CountOfFilesAndFolders();
-            MovingFolder();
-        }
+            FileInfo fi = new FileInfo(@"D:\VS_Projects\Module8\Module8\Module8\Program.cs"); // Укажем путь
 
-        static void GetCatalogs()
-        {
-            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            using (StreamWriter sw = fi.AppendText())
             {
-                Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                sw.WriteLine($"// Время запуска: {DateTime.Now}");
+            }
 
-                foreach (string d in dirs) // Выведем их все
-                    Console.WriteLine(d);
-
-                Console.WriteLine();
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
-
-                foreach (string s in files)   // Выведем их все
-                    Console.WriteLine(s);
+            // Откроем файл и прочитаем его содержимое
+            using (StreamReader sr = fi.OpenText())
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null)
+                    Console.WriteLine(str);
             }
         }
 
-        static void CountOfFilesAndFolders()
-        {
-            try
+
+
+            static void GetCatalogs()
             {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\" /* Или С:\\ для Windows */ );
-                if (dirInfo.Exists)
+                string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+                if (Directory.Exists(dirName)) // Проверим, что директория существует
                 {
-                    Console.WriteLine("Количество папок и файлов в корневой директории:");
-                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                    Console.WriteLine("Папки:");
+                    string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                    foreach (string d in dirs) // Выведем их все
+                        Console.WriteLine(d);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Файлы:");
+                    string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                    foreach (string s in files)   // Выведем их все
+                        Console.WriteLine(s);
                 }
-
-                DirectoryInfo newDirectory = new DirectoryInfo(@"D:\newDirectory");
-                if (!newDirectory.Exists)
-                    newDirectory.Create();
-
-                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
-
-                Console.WriteLine($"Название каталога: {dirInfo.Name}");
-                Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
-                Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-                Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
-
-                DeleteFolder();
-                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
-
             }
-            catch (Exception e)
+
+            static void CountOfFilesAndFolders()
             {
-                Console.WriteLine(e.Message);
-            }
-        }
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(@"D:\" /* Или С:\\ для Windows */ );
+                    if (dirInfo.Exists)
+                    {
+                        Console.WriteLine("Количество папок и файлов в корневой директории:");
+                        Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                    }
 
-        static void DeleteFolder()
-        {
-            try
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\newDirectory");
-                dirInfo.Delete(true); // Удаление со всем содержимым
-                Console.WriteLine("Каталог удален");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+                    DirectoryInfo newDirectory = new DirectoryInfo(@"D:\newDirectory");
+                    if (!newDirectory.Exists)
+                        newDirectory.Create();
 
-        static void MovingFolder()
-        {
-            try
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\alena\Desktop\Basket");
-                string trashPath = @"C:\Users\alena\Desktop\Basket";
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
 
-                dirInfo.MoveTo(trashPath);
+                    Console.WriteLine($"Название каталога: {dirInfo.Name}");
+                    Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+                    Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+                    Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+
+                    DeleteFolder();
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
-            catch (Exception e)
+
+            static void DeleteFolder()
             {
-                Console.WriteLine(e.Message);
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(@"D:\newDirectory");
+                    dirInfo.Delete(true); // Удаление со всем содержимым
+                    Console.WriteLine("Каталог удален");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-        }
+
+            static void MovingFolder()
+            {
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\alena\Desktop\Basket");
+                    string trashPath = @"C:\Users\alena\Desktop\Basket";
+
+                    dirInfo.MoveTo(trashPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
     }
 }
+// Время последнего запуска: 18.01.2021 21:23:55
+// Время последнего запуска: 18.01.2021 21:25:26
+// Время запуска: 18.01.2021 21:27:16
